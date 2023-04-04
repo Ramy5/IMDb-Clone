fb7c980c7f4e571fc85517295fb601d7168bb8c5
RamySapry <ramyjounior464@gmail.com>
2023-04-04 15:33:06 +0200

    install tailwind css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Hello my world!</div>;
+  return (
+    <h1 className="text-red-600 text-3xl font-bold underline">
+      Hello my world!
+    </h1>
+  );
 }
