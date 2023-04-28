340840233e187469d075be8197d296cfd8d1e0a3
RamySapry <ramyjounior464@gmail.com>
2023-04-28 11:32:50 +0300

    Some improvement in main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,41 +1,33 @@
 import Pagination from "@/components/Pagination/Pagination";
 import Results from "@/components/ResultsData/Results";
 
-const API_KEY = process.env.API_KEY;
-
 const Home = async ({ searchParams }) => {
-  try {
-    const page = searchParams.page || "fetchTrending";
-    const pageNumber = searchParams.pageNumber || 1;
+  const page = searchParams.page || "fetchTrending";
+  const pageNumber = searchParams.pageNumber || 1;
 
-    // QUERY FOR FETCHING DATA
-    const language = "en-Us";
-    let pageParamsCheck;
+  // QUERY FOR FETCHING DATA
+  let pageParamsCheck;
 
-    if (page === "fetchTopRated") pageParamsCheck = "movie/top_rated";
-    else if (page === "fetchNew") pageParamsCheck = "movie/now_playing";
-    else if (page === "fetchTrending") pageParamsCheck = "trending/movie/week";
+  if (page === "fetchTopRated") pageParamsCheck = "movie/top_rated";
+  else if (page === "fetchNew") pageParamsCheck = "movie/now_playing";
+  else if (page === "fetchTrending") pageParamsCheck = "trending/movie/week";
 
-    const res = await fetch(
-      `https://api.themoviedb.org/3/${pageParamsCheck}?api_key=${API_KEY}&language=${language}&page=${pageNumber}`,
-      { next: { revalidate: 10000 } }
-    );
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${pageParamsCheck}?api_key=${process.env.API_KEY}&language=en-Us&page=${pageNumber}`
+  );
 
-    if (!res.ok) throw new Error("Failed to fetch data!");
+  if (!res.ok) throw new Error("Failed to fetch data!");
 
-    const data = await res.json();
+  const data = await res.json();
 
-    return (
-      <div>
-        <Results results={data?.results} />
-        {data?.total_results >= 1 && (
-          <Pagination totalPages={data?.total_pages} />
-        )}
-      </div>
-    );
-  } catch (err) {
-    throw new Error(err.message);
-  }
+  return (
+    <>
+      <Results results={data?.results} />
+      {data?.total_results >= 1 && (
+        <Pagination totalPages={data?.total_pages} />
+      )}
+    </>
+  );
 };
 
 export default Home;
